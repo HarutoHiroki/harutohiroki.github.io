2a8c27eea449488fe54aa9ad0e25230cdbb7ac87
HarutoHiroki <hiroaki.haruto@gmail.com>
2019-05-26 23:07:13 +0700

    let the nav bar work properly on mobile devices

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ window.onscroll = function() {
   if (prevScrollpos < currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-55px";
+    document.getElementById("navbar").style.top = "-110px";
   }
 };
 
