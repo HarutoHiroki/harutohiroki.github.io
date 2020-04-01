d4c673d71dd66564f15c2fa7324806d737c5203d
HarutoHiroki <hiroaki.haruto@gmail.com>
2020-04-01 14:00:58 +0700

    restore

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ setInterval(function threeBar(){
     document.getElementById("bar3").style.display = "initial"
     document.getElementById("bar4").style.display = "initial"
   } else {
-    document.getElementById("navbar").style.display = "block";
+    document.getElementById("navbar").style.display = "initial";
     document.getElementById("bar1").style.display = "none"
     document.getElementById("bar2").style.display = "none"
     document.getElementById("bar3").style.display = "none"
@@ -59,7 +59,7 @@ setInterval(function threeBar(){
         var e=document.getElementsByClassName("nav-bar");if(e.length){
           var n=e[0],l=n.querySelector(".nav-bars"),t=n.querySelector(".nav-close");
           l&&l.addEventListener("click",()=>{l.classList.toggle("nav--expand"); l.style.display = "none"}),
-          t&&t.addEventListener("click",()=>{l&&l.classList.remove("nav--expand"); l.style.display ="block"})
+          t&&t.addEventListener("click",()=>{l&&l.classList.remove("nav--expand"); l.style.display ="initial"})
         }
       }()
     }))
