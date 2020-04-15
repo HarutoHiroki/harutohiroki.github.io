8ab1309569ea95eab56835a5610a5e7faa09d7bb
HarutoHiroki <hiroaki.haruto@gmail.com>
2020-04-15 10:02:18 +0700

    redo of everything

--- cryptonix/assets/js/main.js
+++ cryptonix/assets/js/main.js
@@ -8,12 +8,6 @@ window.addEventListener('scroll', function() {
 const prevScrollpos = 0;
 window.onscroll = function() {
   scrollFunction()
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos < currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-110px";
-  }
 };
 
 function scrollFunction() {
@@ -23,3 +17,45 @@ function scrollFunction() {
     document.getElementById("myBtn").style.display = "none";
   }
 }
+
+setInterval(function threeBar(){
+  if (document.body.offsetWidth > 505) {
+    document.getElementById("navbar").style.display = "none";
+    document.getElementById("bar1").style.display = "initial"
+    document.getElementById("bar2").style.display = "initial"
+    document.getElementById("bar3").style.display = "initial"
+    document.getElementById("bar4").style.display = "initial"
+  } else {
+    document.getElementById("navbar").style.display = "initial";
+    document.getElementById("bar1").style.display = "none"
+    document.getElementById("bar2").style.display = "none"
+    document.getElementById("bar3").style.display = "none"
+    document.getElementById("bar4").style.display = "none"
+  }
+}, 69)
+
+
+!function(){
+  "use strict";
+  function e(){
+    var e=window.scrollY,n=document.getElementsByClassName("arrow");
+    n.width&&(e>=100?n[0].classList.add("arrow--scrolled"):n[0].classList.remove("arrow--scrolled"))
+  }
+  function n(){
+    var e=window.innerHeight,n=document.querySelectorAll(".scroll-in:not(.scroll-in--scrolled)");
+    n.length&&Array.from(n).forEach(n=>{n.getBoundingClientRect().y<e&&n.classList.add("scroll-in--scrolled")})
+  }
+  e(),
+  window.addEventListener("scroll",(function(){e(),n()})),
+  window.addEventListener("load",(
+    function(){
+      n(),
+      function(){
+        var e=document.getElementsByClassName("nav-bar");if(e.length){
+          var n=e[0],l=n.querySelector(".nav-bars"),t=n.querySelector(".nav-close");
+          l&&l.addEventListener("click",()=>{l.classList.toggle("nav--expand"); l.style.display = "none"}),
+          t&&t.addEventListener("click",()=>{l&&l.classList.remove("nav--expand"); l.style.display ="block"})
+        }
+      }()
+    }))
+  }();
