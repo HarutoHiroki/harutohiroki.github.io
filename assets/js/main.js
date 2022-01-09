e9fca2d42c8ef9ac4a53b746b8f849109656a617
HarutoHiroki <hiroaki.haruto@gmail.com>
2022-01-09 17:02:03 +0700

    final (probably not) changes :)

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,32 +40,17 @@ setInterval(function threeBar(){
 }, 69)
 
 
-!function(){
-  "use strict";
-  function e(){
-    var e=window.scrollY,n=document.getElementsByClassName("arrow");
-    n.width&&(e>=100?n[0].classList.add("arrow--scrolled"):n[0].classList.remove("arrow--scrolled"))
+window.addEventListener("load",(
+  function(){
+    var e=document.getElementsByClassName("nav-bar");if(e.length){
+      var n=e[0],l=n.querySelector(".nav-bars"),t=n.querySelector(".nav-close");
+      l&&l.addEventListener("click",()=>{l.style.display = "none"; l.classList.toggle("nav--expand");}),
+      t&&t.addEventListener("click",()=>{
+        l&&l.classList.remove("nav--expand"); 
+        setTimeout(function() {
+          l.style.display ="block";
+        }, 175);
+      })
+    }
   }
-  function n(){
-    var e=window.innerHeight,n=document.querySelectorAll(".scroll-in:not(.scroll-in--scrolled)");
-    n.length&&Array.from(n).forEach(n=>{n.getBoundingClientRect().y<e&&n.classList.add("scroll-in--scrolled")})
-  }
-  e(),
-  window.addEventListener("scroll",(function(){e(),n()})),
-  window.addEventListener("load",(
-    function(){
-      n(),
-      function(){
-        var e=document.getElementsByClassName("nav-bar");if(e.length){
-          var n=e[0],l=n.querySelector(".nav-bars"),t=n.querySelector(".nav-close");
-          l&&l.addEventListener("click",()=>{l.style.display = "none"; l.classList.toggle("nav--expand");}),
-          t&&t.addEventListener("click",()=>{
-            l&&l.classList.remove("nav--expand"); 
-            setTimeout(function() {
-              l.style.display ="block";
-            }, 175);
-          })
-        }
-      }()
-    }))
-  }();
+));
