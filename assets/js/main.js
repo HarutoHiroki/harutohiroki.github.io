153a30d39abe8799bfa4fcc2a713b7537c316e1e
HarutoHiroki <hiroaki.haruto@gmail.com>
2022-01-15 10:07:05 +0700

    update for future video background

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
 setInterval(function threeBar(){
   if (document.body.offsetWidth > 505) {
     document.getElementById("navbar").style.display = "none";
-    document.getElementById("bar1").style.display = "initial"
-    document.getElementById("bar2").style.display = "initial"
-    document.getElementById("bar3").style.display = "initial"
-    document.getElementById("bar4").style.display = "initial"
+    document.getElementById("bar1").style.display = "initial";
+    document.getElementById("bar2").style.display = "initial";
+    document.getElementById("bar3").style.display = "initial";
+    document.getElementById("bar4").style.display = "initial";
   } else {
     document.getElementById("navbar").style.display = "initial";
-    document.getElementById("bar1").style.display = "none"
-    document.getElementById("bar2").style.display = "none"
-    document.getElementById("bar3").style.display = "none"
-    document.getElementById("bar4").style.display = "none"
+    document.getElementById("bar1").style.display = "none";
+    document.getElementById("bar2").style.display = "none";
+    document.getElementById("bar3").style.display = "none";
+    document.getElementById("bar4").style.display = "none";
   }
-}, 69)
+}, 69);
 
 
 window.addEventListener("load",(
@@ -21,11 +21,16 @@ window.addEventListener("load",(
       var n=e[0],l=n.querySelector(".nav-bars"),t=n.querySelector(".nav-close");
       l&&l.addEventListener("click",()=>{l.style.display = "none"; l.classList.toggle("nav--expand");}),
       t&&t.addEventListener("click",()=>{
-        l&&l.classList.remove("nav--expand"); 
+        l&&l.classList.remove("nav--expand"); // jshint ignore:line
         setTimeout(function() {
           l.style.display ="block";
         }, 175);
-      })
+      }); // jshint ignore:line
     }
   }
 ));
+
+/*
+window.onfocus = function(){document.getElementById("home-video").play();};
+window.onblur = function(){document.getElementById("home-video").pause();};
+*/
